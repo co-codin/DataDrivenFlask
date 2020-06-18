827ed19ca9cb844e1cf1af71056d0b0978189a83
ElijahCoding <cuiyeqing960904@gmail.com>
2020-06-18 16:32:51 +0300

    example built

--- routing/app.py
+++ routing/app.py
@@ -14,9 +14,11 @@ def main():
 def register_blueprints():
     from routing.views import home_views
     from routing.views import package_views
+    from routing.views import cms_views
 
     app.register_blueprint(home_views.blueprint)
     app.register_blueprint(package_views.blueprint)
+    app.register_blueprint(cms_views.blueprint)
 
 if __name__ == '__main__':
     main()
--- routing/services/cms_service.py
+++ routing/services/cms_service.py
@@ -8,3 +8,11 @@ fake_db = {
         'page_details': 'Details about company employees ...',
     },
 }
+
+def get_page(url: str) -> dict:
+    if not url:
+        return {}
+    url = url.strip().lower()
+    url = '/' + url.lstrip('/')
+    page = fake_db.get(url, {})
+    return page
