713f2fe65ce27c46bfebfbb03e573de4d6db8b7b
ElijahCoding <cuiyeqing960904@gmail.com>
2020-06-18 16:20:52 +0300

    building cms structure

--- routing/app.py
+++ routing/app.py
@@ -13,7 +13,10 @@ def main():
 
 def register_blueprints():
     from routing.views import home_views
+    from routing.views import package_views
+
     app.register_blueprint(home_views.blueprint)
+    app.register_blueprint(package_views.blueprint)
 
 if __name__ == '__main__':
     main()
---
+++ routing/services/cms_service.py
@@ -0,0 +1,10 @@
+fake_db = {
+    '/company/history': {
+        'page_title': 'Company history',
+        'page_details': 'Details about company history...',
+    },
+    '/company/employees': {
+        'page_title': 'Our team',
+        'page_details': 'Details about company employees ...',
+    },
+}
