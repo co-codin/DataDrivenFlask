8ff3e56f469946fe71dbf62c659e1ff9239acbd5
ElijahCoding <cuiyeqing960904@gmail.com>
2020-08-18 00:44:24 +0300

    building models

--- quiz/quiz/__init__.py
+++ quiz/quiz/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from .extensions import db, login_manager
 
 def create_app(config_file='settings.py'):
     app = Flask(__name__)
     
     app.config.from_pyfile(config_file)
+
+    db.init_app(app)
+
+    login_manager.init_app(app)
 
     return app
