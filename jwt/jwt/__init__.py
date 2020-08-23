d075039430253c55f8fd46db35a6cfc165d8bdb4
ElijahCoding <cuiyeqing960904@gmail.com>
2020-08-23 15:39:17 +0300

    built

--- jwt/jwt/__init__.py
+++ jwt/jwt/__init__.py
@@ -1,19 +1,18 @@
 from flask import Flask
-import os
-
-from .extensions import db, guard
-from .models import User
+from os import environ
 
 from .commands import create_users, create_database
-
+from .extensions import db, guard
+from .models import User
 from .routes import api
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+    app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SQLALCHEMY_DATABASE_URI')
+    app.config['JWT_ACCESS_LIFESPAN'] = {'minutes': 1}
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     guard.init_app(app, User)
--- jwt/jwt/routes.py
+++ jwt/jwt/routes.py
@@ -1,21 +1,29 @@
 from flask import Blueprint, jsonify, request
+from flask_praetorian import auth_required
 from .extensions import guard
 
 api = Blueprint('api', __name__)
 
-@api.route('/login')
+@api.route('/login', methods=['POST'])
 def login():
     json_data = request.get_json()
     username = json_data['username']
     password = json_data['password']
 
     user = guard.authenticate(username, password)
-
-    return jsonify({'access_token': guard.encode_jwt_token(user)})
+    token = guard.encode_jwt_token(user)
+    return jsonify({'access_token' : token})
 
 @api.route('/protected')
+@auth_required
 def protected():
-    return None
+    return jsonify({'result' : 'You are in a special area!'})
+
+@api.route('/refresh')
+def refresh():
+    json_data = request.get_json()
+    token = guard.refresh_jwt_token(json_data['token'])
+    return jsonify({'access_token': token})
 
 @api.route('/open')
 def open():
