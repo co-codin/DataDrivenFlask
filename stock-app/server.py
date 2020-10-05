273de297d5213bb6f56e35ac97df2944d8b99e47
ElijahCoding <cuiyeqing960904@gmail.com>
2020-10-05 14:59:50 +0300

    templating , styling

--- stock-app/server.py
+++ stock-app/server.py
@@ -1,16 +1,27 @@
-from flask import Flask
 import requests
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 def fetch_price(ticker):
     data = requests.get('https://financialmodelingprep.com/api/v3/stock/real-time-price/{}'.format(ticker.upper()),
-                        params={
-                            'apikey': 'demo'
-                        }).json()
-    return data['price']
+                        params={'apikey': 'demo'}).json()
+    return data["price"]
 
-@app.route('/stock/<ticker>')
-def stock(ticker):
-    price = fetch_price(ticker)
-    return 'The price of {ticker} is {price}'.format(ticker=ticker, price=price)
+@app.route('/stocks/<string:ticker>')
+def view_stock(ticker):
+    stock_price = fetch_price(ticker)
+    return render_template('stock_quote.html', ticker=ticker, stock_price=stock_price)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/lookup', methods=['POST'])
+def lookup():
+    return redirect(url_for('view_stock', ticker=request.form["ticker"]))
+
+@app.route('/get_lookup', methods=['GET'])
+def get_lookup():
+    return redirect(url_for('view_stock', ticker=request.args["ticker"]))
+
