b03eb4e17c037a0a86434f4fc27a65a37f1206ca
ElijahCoding <cuiyeqing960904@gmail.com>
2021-06-15 00:55:38 +0300

    link templates

--- FlaskTemplate/website/auth.py
+++ FlaskTemplate/website/auth.py
@@ -1,4 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
+
+@auth.route('/login')
+def login():
+    return 'login'
+
+
+@auth.route('/logout')
+def logout():
+    return 'logout'
+
+
+@auth.route('sign-up')
+def sign_up():
+    return render_template('sign_up.html')
